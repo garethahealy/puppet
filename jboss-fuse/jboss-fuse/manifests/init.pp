74eee4e75b2a618b7c28e669ad858f828c52564d
Gareth Healy <garethahealy@gmail.com>
2014-09-04 17:24:41 +0100

    First draft

---
+++ jboss-fuse/jboss-fuse/manifests/init.pp
@@ -0,0 +1,55 @@
+# /etc/puppet/modules/jboss-fuse/manifests/init.pp
+
+class jboss-fuse {
+
+	$PUPPET_MODULES_HOME = "/etc/puppet/modules"
+	
+	$FUSE_FILENAME_ZIP = "jboss-fuse-full-6.1.0.redhat-379.zip"
+	$FUSE_FILENAME = "jboss-fuse-6.1.0.redhat-379"
+	$RH_HOME = "/opt/rh1"
+
+	$DEFAULT_FUSE_USERNAME = "admin"
+	$DEFAULT_FUSE_PASSWORD = "admin"
+
+	$USER_USERNAME = "jbossfuse"
+
+	#Create fuse group
+	group { "${USER_USERNAME}":
+		ensure  => present,
+  		name	=> "${USER_USERNAME}",
+		system 	=> true
+	}
+
+	#Create fuse user
+	user { "${USER_USERNAME}":
+      		ensure	=> present,
+		name	=> "${USER_USERNAME}",
+      		gid     => "${USER_USERNAME}",
+		system	=> true
+    	}
+	
+	#Create root directory for JBoss-Fuse 
+	file { ["${RH_HOME}"]:
+		ensure => "directory",
+        	owner  => "${USER_USERNAME}",
+        	group  => "${USER_USERNAME}",
+        	mode   => "0755"
+	}
+	
+	#Unzip fuse 
+	exec { "unzip ${PUPPET_MODULES_HOME}/jboss-fuse/files/${FUSE_FILENAME_ZIP}":
+    		cwd 	=> "${RH_HOME}",
+    		creates => "${RH_HOME}/${FUSE_FILENAME}/bin/fuse",
+		path    => ["/usr/bin", "/usr/sbin"],
+		user	=> "${USER_USERNAME}",
+		require => File["${RH_HOME}"]
+  	}
+	
+	#Append the default user
+	file_line { "users.properties":
+		ensure	=> present,
+		path 	=> "${RH_HOME}/${FUSE_FILENAME}/etc/users.properties",
+		line 	=> "\n${DEFAULT_FUSE_USERNAME}=${DEFAULT_FUSE_PASSWORD},admin",
+		require	=> Exec["unzip ${PUPPET_MODULES_HOME}/jboss-fuse/files/${FUSE_FILENAME_ZIP}"]
+	}
+}

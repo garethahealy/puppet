27dbae7624bfbbcb4203bec8732ac6140547f912
Gareth Healy <garethahealy@gmail.com>
2016-01-27 12:18:12 +0000

    Updated scaffolding scripts version

--- jbossfuse621/files/run.sh
+++ jbossfuse621/files/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp /opt/garethahealy/puppet/modules/jbossfuse621/files/jboss-fuse-full-6.2.1.redhat-084.zip /opt/rh/jboss-fuse-full-6.2.1.redhat-084.zip
-cp /opt/garethahealy/puppet/modules/jbossfuse621/files/scaffolding-scripts-1.0.3-all.zip /tmp/scaffolding-scripts-1.0.3-all.zip
+cp /opt/garethahealy/puppet/modules/jbossfuse621/files/scaffolding-scripts-1.0.4-all.zip /tmp/scaffolding-scripts-1.0.4-all.zip
 
 cd /tmp &&
     unzip -o scaffolding-scripts-*-all.zip &&
